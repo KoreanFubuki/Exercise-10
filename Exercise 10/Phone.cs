b042d9bb95e62724c0a3b09522a362c0d09ae853
KoreanFubuki <145092552+KoreanFubuki@users.noreply.github.com>
2023-09-19 16:44:59 +0400

    Add project files.

---
+++ Exercise 10/Phone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    internal class Phone
+    {
+        private string _Company;
+        private string _Model;
+        private string _ReleaseDay;
+        public Phone()
+        {
+            _Company = "Unknown";
+            _Model = "Unknown";
+            _ReleaseDay = "Unknown";
+        }
+        public Phone(string company = "Samsung", string model = "A21")
+        {
+            _Company = company;
+            _Model = model;
+            _ReleaseDay = "Unknown";
+        }
+        public Phone(string company = "Iphone", string model = "13 Pro Max", string releaseday = "9 November")
+        {
+            _Company = company;
+            _Model = model;
+            _ReleaseDay = releaseday;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine("It is {0} created by {1}. It was released {2}.", _Model, _Company, _ReleaseDay);
+        }
+    }
+}
---
+++ Exercise 10/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercise_10;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone something = new Phone();
+            Phone something2 = new Phone("Samsung", "A21");
+            Phone something3 = new Phone("Iphone", "Pro Max 13", "9th of November");
+            something.Introduce();
+            something2.Introduce();
+            something3.Introduce();
+        }
+    }
+}
